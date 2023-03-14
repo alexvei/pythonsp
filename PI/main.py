8a3f471bbe500d8490d7bee378d6dc31ed8b0a7c
alexvei <vasilalexpanta@gmail.com>
2023-03-14 12:56:15 +0000

    Reorganize, implement functinos

--- PI/main.py
+++ PI/main.py
@@ -1,23 +1,33 @@
 import numpy as np
 from decimal import *
 
-# **Find PI to the Nth Digit** - 
 
-# Enter a number and have the program generate &pi; 
-number = input("Enter number: ")
-limits = 1
-while(limits):
-       while(not number.isdigit()):
-              print("Please enter a number.")
-              number = input("Enter number: ")
-       number1 = int(number)
-       if (number1 >= 1 and number1 <= 48):
-              limits = 0
-       else:
+def get_prompt():
+       while True:
+              number_string = input("Enter number: ")
+              if number_string.isdigit():
+                     number = int(number_string)
+                     if 1 <= number <= 48:
+                            return number
               print("Enter a number between 1-48 (inclusive).")
-              number = 'a'
-getcontext().prec = number1+1
-print(f"The number pi up to {number1} decimal places is: {Decimal(np.pi)/Decimal(1)}.")
 
 
+def pi_calc(number):
+       try:
+              getcontext().prec = number + 1
+              pi = Decimal(np.pi) / Decimal(1)
+              return pi
+       except InvalidOperation:
+              print("Error: requested precision too large.")
+              return None
 
+
+def main():
+       number = get_prompt()
+       pi = pi_calc(number)
+       if pi is not None:
+              print(f"The number pi up to {number} decimal places is: {pi}.")
+
+
+if __name__ == "__main__":
+       main()
