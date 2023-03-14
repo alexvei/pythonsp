979d367b1107cb78b6022a2ae843633b5f2587f6
alexvei <vasilalexpanta@gmail.com>
2023-03-14 11:46:15 +0000

    PI mini-project

---
+++ PI/main.py
@@ -0,0 +1,12 @@
+
+digit = 0
+while ((digit >= 8) or (digit < 1)):
+	digit = int(input("Enter number of decimal places: "))
+	if digit.isdigit():
+
+	if digit >= 8 or digit < 1:
+		print ("Please only enter a number between 1 and 8 (inclusive)\n")
+
+
+
+print(f"So you want up to {digit} decimal places.\n")
