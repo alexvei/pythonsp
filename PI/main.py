4b762302321bad75e84a4f2dff6cb08d288cc03c
alexvei <vasilalexpanta@gmail.com>
2023-03-14 13:00:16 +0000

    PI mini-project finished, with comments

--- PI/main.py
+++ PI/main.py
@@ -2,6 +2,7 @@ import numpy as np
 from decimal import *
 
 
+# Prompt and checking.
 def get_prompt():
        while True:
               number_string = input("Enter number: ")
@@ -12,16 +13,18 @@ def get_prompt():
               print("Enter a number between 1-48 (inclusive).")
 
 
+
+# Get the decimal places prompted.
 def pi_calc(number):
        try:
               getcontext().prec = number + 1
-              pi = Decimal(np.pi) / Decimal(1)
+              pi = Decimal(np.pi) / Decimal(1) # If I don't divide by 1, decimal module doesn't give me appropriate decimal places.
               return pi
-       except InvalidOperation:
-              print("Error: requested precision too large.")
+       except InvalidOperation:    # Using try-except, a bit overkill here, but good practice.
+              print("Error: requested precision too large.")  
               return None
 
-
+# Entire script runs on a simple main function.
 def main():
        number = get_prompt()
        pi = pi_calc(number)
@@ -29,5 +32,6 @@ def main():
               print(f"The number pi up to {number} decimal places is: {pi}.")
 
 
+# Ensures standalone script.
 if __name__ == "__main__":
        main()
